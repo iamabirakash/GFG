e73f9bb70d1a52c8b16b43422dd693d9bc1970ac
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-06-04 19:37:26 +0530

    Added Reverse an Array

---
+++ ARRAYS/Reverse an Array/answers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int n = arr.length;
+        for(int i=0;i<n/2;i++){
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
+        }
+    }
+}
