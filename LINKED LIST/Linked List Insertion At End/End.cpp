c368c60f107c9e98b2efe3f83493488ca35905a9
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-06-26 09:37:07 +0530

    26th June

---
+++ LINKED LIST/Linked List Insertion At End/End.cpp
@@ -0,0 +1,29 @@
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        Node* newNode = new Node(x);
+        newNode->data = x;
+        newNode->next = NULL;
+        if(head==NULL){
+            head = newNode;
+            return head;
+        } 
+        Node* temp = head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        return head;
+    }
+};
