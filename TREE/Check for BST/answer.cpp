f9c7f9a9f522e942796859842c0add1f69c29a5a
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-07-04 11:32:17 +0530

    4th June

---
+++ TREE/Check for BST/answer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    // Function to check whether a Binary Tree is BST or not.
+    bool isBSTUtil(Node* node, int min, int max) {
+        if (node == nullptr) 
+          	return true;
+
+        if (node->data < min || node->data > max) 
+          	return false;
+        return isBSTUtil(node->left, min, node->data - 1) &&
+               isBSTUtil(node->right, node->data + 1, max);
+    }
+    bool isBST(Node* root) {
+        return isBSTUtil(root, INT_MIN, INT_MAX);
+    }
+};
