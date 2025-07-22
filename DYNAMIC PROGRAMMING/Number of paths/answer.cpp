47b9c75c20884f60301f73269a740af37053d3ff
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-07-22 18:12:17 +0530

    22th July

---
+++ DYNAMIC PROGRAMMING/Number of paths/answer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int paths(vector<vector<int>> &dp,int m,int n){
+        if(m==0 || n==0) return 0;
+        if(m==1 && n==1) return 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        return dp[m][n] = paths(dp,m,n-1)+paths(dp,m-1,n);
+    }
+    int numberOfPaths(int m, int n) {
+        // Code Here
+        vector<vector<int>> dp(m+1,vector<int>(n+1, -1));
+        return paths(dp,m,n);
+    }
+};
