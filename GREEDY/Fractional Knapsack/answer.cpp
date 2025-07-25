6e5e92b31705a619df2b89ce1c6c0f200f96c9d7
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-07-25 11:59:14 +0530

    25th July Added Pair

--- GREEDY/Fractional Knapsack/answer.cpp
+++ GREEDY/Fractional Knapsack/answer.cpp
@@ -11,24 +11,47 @@ class Solution {
     double fractionalKnapsack(vector<int>& values, vector<int>& weights, int W) {
         // code here
         int n = values.size();
-        vector<int> indices(n);
-        for (int i = 0; i < n; i++) indices[i] = i;
-        sort(indices.begin(),indices.end(),[&](int i, int j) {
-            return (double)values[i] / weights[i] > (double)values[j] / weights[j];
-        });
-        double sum = 0.0;
-        int weight = 0;
-        for(int i : indices){
-            if((weight+weights[i])<=W){
-                sum += values[i];
-                weight += weights[i];
+        vector<pair<double, int>> ratioIdx;
+        for (int i = 0; i < n; ++i) {
+            ratioIdx.emplace_back((double)values[i] / weights[i], i);
+        }
+        sort(ratioIdx.rbegin(), ratioIdx.rend());
+
+        double totalValue = 0.0;
+        int currWeight = 0;
+        for (auto& p : ratioIdx) {
+            int idx = p.second;
+            if (currWeight + weights[idx] <= W) {
+                totalValue += values[idx];
+                currWeight += weights[idx];
             } else {
-                int extra = W - weight;
-                double adds = ((double)values[i] / weights[i]) * extra;
-                sum += adds;
+                int remain = W - currWeight;
+                totalValue += ((double)values[idx] / weights[idx]) * remain;
                 break;
             }
         }
-        return sum;
+
+        return totalValue;
+        
+        // int n = values.size();
+        // vector<int> indices(n);
+        // for (int i = 0; i < n; i++) indices[i] = i;
+        // sort(indices.begin(),indices.end(),[&](int i, int j) {
+        //     return (double)values[i] / weights[i] > (double)values[j] / weights[j];
+        // });
+        // double sum = 0.0;
+        // int weight = 0;
+        // for(int i : indices){
+        //     if((weight+weights[i])<=W){
+        //         sum += values[i];
+        //         weight += weights[i];
+        //     } else {
+        //         int extra = W - weight;
+        //         double adds = ((double)values[i] / weights[i]) * extra;
+        //         sum += adds;
+        //         break;
+        //     }
+        // }
+        // return sum;
     }
 };
