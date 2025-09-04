0dfda4b7cef485324e2d107463c6f0b1604ae7a7
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-09-04 10:15:19 +0530

    4th Sep

---
+++ LINKED LIST/Linked List Group Reverse/answer.cpp
@@ -0,0 +1,33 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node reverseKGroup(Node head, int k) {
+        // code here
+        Node curr = head;
+        Node prev = null;
+        Node next = null;
+        int count = 0;
+        while (curr != null && count < k) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        if(next != null) {
+            head.next = reverseKGroup(next, k);
+        }
+        return prev;
+    }
+}
